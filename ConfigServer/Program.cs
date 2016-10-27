ed8c64b3dbf433b1b45520424030ded6aff30737
심혜진 <hj12334@naver.com>
2016-10-27 17:31:18 +0900

    add protocol

---
+++ ConfigServer/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfigServer
+{
+    class Program
+    {
+        private static bool isclosing = false;
+        static SocketManager conn;
+
+        static void Main(string[] args)
+        {
+            SetConsoleCtrlHandler(ConsoleCtrlCheck, true);
+
+            int port = int.Parse(ConfigurationManager.AppSettings.Get("port"));
+
+            conn = new SocketManager(port);
+            conn.Start();
+        }
+
+        #region unmanaged
+        // Declare the SetConsoleCtrlHandler function
+        // as external and receiving a delegate.
+
+        [DllImport("Kernel32")]
+        public static extern bool SetConsoleCtrlHandler(HandlerRoutine Handler, bool Add);
+
+        // A delegate type to be used as the handler routine
+        // for SetConsoleCtrlHandler.
+        public delegate bool HandlerRoutine(CtrlTypes CtrlType);
+
+        // An enumerated type for the control messages
+        // sent to the handler routine.
+        public enum CtrlTypes
+        {
+            CTRL_C_EVENT = 0,
+            CTRL_BREAK_EVENT,
+            CTRL_CLOSE_EVENT,
+            CTRL_LOGOFF_EVENT = 5,
+            CTRL_SHUTDOWN_EVENT
+        }
+
+        #endregion
+
+        private static bool ConsoleCtrlCheck(CtrlTypes ctrlType)
+        {
+            conn.Stop();
+
+            switch (ctrlType)
+            {
+                case CtrlTypes.CTRL_C_EVENT:
+                    isclosing = true;
+                    Environment.Exit(0);
+                    break;
+
+                case CtrlTypes.CTRL_BREAK_EVENT:
+                    isclosing = true;
+                    Console.WriteLine("CTRL+BREAK received!");
+                    break;
+
+                case CtrlTypes.CTRL_CLOSE_EVENT:
+                    isclosing = true;
+                    Console.WriteLine("Program being closed!");
+                    Environment.Exit(0);
+                    break;
+
+                case CtrlTypes.CTRL_LOGOFF_EVENT:
+                case CtrlTypes.CTRL_SHUTDOWN_EVENT:
+                    isclosing = true;
+                    Console.WriteLine("User is logging off!");
+                    break;
+            }
+            return true;
+        }
+    }
+}
---
+++ ConfigServer/Protocol.cs
@@ -0,0 +1,36 @@
+﻿namespace ConfigServer
+{
+    struct Header
+    {
+        int lenght;
+        int srcType;    //0 아무값....
+        int srcCode;    //0 똥값....
+        int dstType;    //0 쓰레기값....
+        int dstCode;    //0 가비지값....
+    }
+    struct Body
+    {
+        Command comm;
+        byte[] data;
+    }
+
+    enum Command
+    {
+        MATCH_REQUEST,
+        MATCH_COMPLET,
+        LATENCY,
+        HEALTH_CHECK,           //Config Server가 사용할 수 있는 것
+        MSLIST_REQUEST          //Config Server가 사용할 수 있는 것
+    }
+
+    enum DestinationType:int    //필요없는 거..................
+    {
+        MATCHING_SERVER = 0,
+        MATCHING_CLIENT,
+        ROOM_MANAGER,
+        PACKET_GENERATOR,
+        MONITORING_SERVER
+    };
+
+
+}
